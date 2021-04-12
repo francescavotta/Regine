d3e07dd13ae66e315388ddae31ade3ef79b57616
Utente <Utente@NBASUS.homenet.telecomitalia.it>
2021-04-12 09:55:28 +0200

    Soluzione con TUTTE le soluzioni

--- src/main/java/it/polito/tdp/regine/model/Regine.java
+++ src/main/java/it/polito/tdp/regine/model/Regine.java
@@ -18,26 +18,26 @@ public class Regine {
 	//     [0, 2]
 	//            [0, 2, 1]
 	private int N;
-	private List<Integer> soluzione;
+	private List<List<Integer>> soluzioni;
 	
-	public List<Integer> risolvi(int N){
+	public List<List<Integer>> risolvi(int N){
 		this.N = N;
 		List <Integer> parziale = new ArrayList<Integer>(); //FACCIO get() QUINDI ARRAYLIST!!
-		this.soluzione=null; //se non ho soluzioni restituisco null
+		this.soluzioni=new ArrayList<>(); //se non ho soluzioni restituisco null
 		
 		//caso iniziale
 		cerca(parziale, 0);
 		
-		return this.soluzione;	
+		return this.soluzioni;	
 	}
 	
 	// cerca == true : trovato; cerca == false: cerca ancora
-	private boolean cerca(List<Integer>parziale, int livello) {
+	private void cerca(List<Integer>parziale, int livello) {
 		if(livello==N) {
 			// caso terminale
 			//System.out.println(parziale);
-			this.soluzione = new ArrayList<Integer>(parziale);
-			return true; //al livello 8 restituisco vero, ho la soluzione
+			this.soluzioni.add(new ArrayList<Integer>(parziale));
+			//return true; //al livello 8 restituisco vero, ho la soluzione
 		} else {
 			for(int colonna=0; colonna<N; colonna++) {//<- tutte le posizioni per mettere la regina
 				// if la mossa nella casella [livello][colonna] è valida
@@ -45,16 +45,17 @@ public class Regine {
 			
 				if(posValida(parziale, colonna)) {
 					parziale.add(colonna);//aggiungo a parziale
-					boolean trovato = cerca(parziale, livello+1);
-					if(trovato)//se ho la soluzione, ritorno tutto vero
-						return true;
+					//boolean trovato = 
+						cerca(parziale, livello+1);
+					//if(trovato)//se ho la soluzione, ritorno tutto vero
+						//return true;
 					parziale.remove(parziale.size()-1); //backtracking, quando risalgo di livello devo mettere le cose a posto!
 				
 					//posso fare una nuova lista, parzialeNuovo, che contenga parziale e faccio add
 					//è meno efficiente perchè devo copiare un arraylist in un altro per aggiungerne uno.
 				}
 			}
-			return false;//non ho trovato neppure io la soluzione
+			//return false;//non ho trovato neppure io la soluzione
 		}
 	}
 
